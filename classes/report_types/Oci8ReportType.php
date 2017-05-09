2a4a4df19c5c87cfc456f4cc5af05d7020c3cf48
Schtritoff <tstritof@gmail.com>
2017-05-09 10:06:37 +0200

    added support for Oracle NLS globalization options

--- classes/report_types/Oci8ReportType.php
+++ classes/report_types/Oci8ReportType.php
@@ -39,10 +39,20 @@ class Oci8ReportType extends PdoReportType {
 			if(isset($config['user_rw'])) $username = $config['user_rw'];
 			if(isset($config['pass_rw'])) $password = $config['pass_rw'];
 		}
-        
-        //putenv('NLS_LANG=CROATIAN_CROATIA.AL32UTF8');
-		$report->conn = new \Yajra\Pdo\Oci8($dsn,$username,$password);
 
+		// create PDO_OCI8 object
+		$report->conn = new \Yajra\Pdo\Oci8($dsn,$username,$password);
 		$report->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		// apply Oracle NLS settings if configured
+		if (array_key_exists("nls_options", $config) && is_array($config["nls_options"]) && sizeof($config["nls_options"])>0) {
+			$sql_nls = "ALTER SESSION SET ";
+			foreach	($config["nls_options"] as $field => $value) {
+				$sql_nls .= " {$field} = '{$value}' ";
+			}
+			$stmt = $report->conn->prepare($sql_nls);
+			$stmt->execute();
+		}
+
 	}
 }
